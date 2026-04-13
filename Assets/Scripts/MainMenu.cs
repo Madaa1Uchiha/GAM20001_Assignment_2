383792d81efc08e6d8551359d072baef209eaf36
SamMacciocca <1033512@student.sae.edu.au>
2026-04-13 11:55:47 +1000

    Menu
    Added Menu with ability to load scene and a volume slider and exit application.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Debug.Log ("Quit");
+        #if UNITY_EDITOR
+        // This stops Play Mode in the Unity Editor
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            // This closes the actual built application
+            Application.Quit();
+        #endif
     }
 }
