14278ae7637a6541bcaef627ce5d88d941a9f0de
Sam <sammacciocca@gmail.com>
2026-04-20 15:29:39 +1000

    Fixes
    Tried to fix res and fullscreen but resolution is not picking up res sizes aand fullscreen not working as well

--- Assets/Scripts/S_GameSettings.cs
+++ Assets/Scripts/S_GameSettings.cs
@@ -1,19 +1,21 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEditor;
 using System.Collections.Generic;
+
 public class GameSettings : MonoBehaviour
 {   
     public TMP_Dropdown ResDropDown;    
     public Toggle FullScreenToggle;    
     private int SelectedResolution;
     private bool IsFullscreen;
+    Resolution[] AllResolutions;
+    List<Resolution> SelectedResolutionList = new List<Resolution>();
 
     public void ChangeScreenSize()
     {
-       IsFullscreen = FullScreenToggle.isOn;
-       Screen.SetResolution(SelectedResolutionList[SelectedResolution].width, SelectedResolutionList[SelectedResolution].height, IsFullscreen);
+        IsFullscreen = FullScreenToggle.isOn;
+        Screen.SetResolution(SelectedResolutionList[SelectedResolution].width, SelectedResolutionList[SelectedResolution].height, IsFullscreen);
     }
 
     public void ChangeResolution()
@@ -22,27 +24,42 @@ public class GameSettings : MonoBehaviour
         Screen.SetResolution(SelectedResolutionList[SelectedResolution].width, SelectedResolutionList[SelectedResolution].height, IsFullscreen);
     }
 
-    Resolution[] AllResolutions;
-    List<Resolution> SelectedResolutionList = new List<Resolution>();
-
     void Start()
     {
-        IsFullscreen = true;
+        Debug.Log("Resolution count: " + Screen.resolutions.Length);
+        IsFullscreen = Screen.fullScreen;
+        FullScreenToggle.isOn = IsFullscreen;
+        Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, IsFullscreen);
+        
         AllResolutions = Screen.resolutions;
-
         List<string> resolutionStringList = new List<string>();
-        string newRes;
+        int currentIndex = 0;
+
         foreach(Resolution res in AllResolutions)
         {
-            newRes = res.width.ToString() +" x " + res.height.ToString();
+            string newRes = res.width.ToString() + " x " + res.height.ToString();
             if(!resolutionStringList.Contains(newRes))
             {
                 resolutionStringList.Add(newRes);
                 SelectedResolutionList.Add(res);
+
+                if(res.width == Screen.currentResolution.width && 
+                   res.height == Screen.currentResolution.height)
+                {
+                    currentIndex = SelectedResolutionList.Count - 1;
+                }
             }
         }
+        if(AllResolutions.Length == 0)
+        {
+            Screen.SetResolution(1920, 1080, IsFullscreen);
+            ResDropDown.AddOptions(new List<string> { "1920 x 1080", "1280 x 720", "800 x 600" });
+            return;
+        }
 
+        ResDropDown.ClearOptions();
         ResDropDown.AddOptions(resolutionStringList);
+        ResDropDown.value = currentIndex;
+        SelectedResolution = currentIndex;
     }
-    
 }
